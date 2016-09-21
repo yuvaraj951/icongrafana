734f613b89fc9b6a81c58dd41cc777559ca10814
yuvaraj <yuvarajd951@gmail.com>
2016-09-21 17:30:18 +0530

    Malfunction alerts &maintenance alerts of activities commited

---
+++ pkg/services/sqlstore/malfunalert_activity.go
@@ -0,0 +1,54 @@
+package sqlstore
+
+import (
+  "github.com/grafana/grafana/pkg/bus"
+  m "github.com/grafana/grafana/pkg/models"
+  "github.com/go-xorm/xorm"
+
+  "github.com/grafana/grafana/pkg/log"
+  // "fmt"
+  //"github.com/Unknwon/bra/cmd"
+
+  "time"
+)
+
+func init()  {
+
+  bus.AddHandler("sql",addMaintenanceAlerts)
+  bus.AddHandler("sql",RemoveMaintenanceUpdate)
+
+}
+
+func addMaintenanceAlerts(cmd *m.AddMalfunalertActivity) error {
+  return inTransaction(func(sess *xorm.Session) error {
+    // check if user exists
+    logger := log.New("main")
+    logger.Info("AddProcessForCurrentOrg222",cmd.OrgId)
+
+
+
+
+    entity := m.MaintenanceActivity{
+      OrgId:   cmd.OrgId,
+      Name:    cmd.Name,
+      Message:cmd.Message,
+      Updated:time.Now(),
+
+    }
+
+    _, err := sess.Insert(&entity)
+    return err
+  })
+}
+
+func RemoveMaintenanceUpdate(cmd *m.DeleteMalfunalertActivity) error {
+  return inTransaction(func(sess *xorm.Session) error {
+    var rawSql = "DELETE FROM malfunalert_activity WHERE org_id=? and id=?"
+    _, err := sess.Exec(rawSql, cmd.OrgId, cmd.Id)
+    if err != nil {
+      return err
+    }
+
+    return validateOneAdminLeftInOrg(cmd.OrgId, sess)
+  })
+}
