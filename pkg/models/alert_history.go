73289874a716914308a955a7563e7536ac7b78c2
yuvaraj <yuvarajd951@gmail.com>
2016-09-26 15:02:21 +0530

    alert history model changing

--- pkg/models/alert_history.go
+++ pkg/models/alert_history.go
@@ -2,13 +2,21 @@ package models
 
 import "time"
 
+type AlertStatus string
+
+const (
+	AlertsPending AlertStatus = "Pending Alerts"
+	AlertsCompleted     AlertStatus = "Completed Alerts"
+
+)
+
 type AlertHistory struct {
 	Id               int64
 	OrgId            int64
 	AlertMessage     string
 	Sended           time.Time
 	Interval         string
-	Status           string
+	Status           AlertStatus
 	Name             string
 	Message          string
 	Updated          time.Time
@@ -21,7 +29,7 @@ type AlertHistoryDTO struct {
 	AlertMessage string     `json:"alertMessage"`
 	Sended       string    `json:"sended"`
 	Interval     string   `json:"interval"`
-	Status       string      `json:"status"`
+	Status       AlertStatus      `json:"status"`
 	Name         string      `json:"name"`
 	Message      string        `json:"Message"`
 	Updated      time.Time       `json:"updated"`
@@ -30,5 +38,10 @@ type AlertHistoryDTO struct {
 }
 type GetAlertHistoryQuery struct {
 	OrgId    int64
+	Result []*AlertHistoryDTO
+}
+type GetAlertHistoryByStatusQuery struct {
+	OrgId    int64
+	Status   AlertStatus
 	Result []*AlertHistoryDTO
 }
